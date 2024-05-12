04a1f870aa4e49be4c20ece061ddb04364f77694
elifsenasoysal <168901373+elifsenasoysal@users.noreply.github.com>
2024-05-12 21:14:48 +0300

    form sayfası başlangıç

---
+++ gonder.php
@@ -0,0 +1,9 @@
+<?php
+    $ad=$_POST["ad"];
+    $soyad=$_POST["soyad"];
+    $e_posta=$_POST["eposta"];
+    $konu=$_POST["konu"];
+    $mesaj=$_POST["mesaj"];
+    $ip=$_SERVER["REMOTE_ADDR"];
+    
+?>
